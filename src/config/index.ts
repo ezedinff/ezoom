dfbece1cec2dda7d3fcd4d1e22dfafb66454df4b
Kintsugi <ezedin.fedlu@gmail.com>
2021-03-28 23:46:46 +0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,4 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyC4j8uj72JPjw7epRVlsX1QEUTRrq7mn6o',
-  authDomain: 'ezoom-4dc6c.firebaseapp.com',
-  projectId: 'ezoom-4dc6c',
-  storageBucket: 'ezoom-4dc6c.appspot.com',
-  messagingSenderId: '632869661658',
-  appId: '1:632869661658:web:bfac588c02d55f4ed0aee3',
-  measurementId: 'G-5R8MQE97XR',
 };
 
 export const ICEservers = {
